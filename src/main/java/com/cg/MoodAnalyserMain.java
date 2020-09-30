30638f83f62f08a4c848aacc91809346cb5e3af2
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-30 17:18:59 +0530

    [Arijit]Added : Welcome message

---
+++ src/main/java/com/cg/MoodAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.cg;
+
+public class MoodAnalyserMain {
+    public static void main(String[] args) {
+    	System.out.println("Mood Analyser Test Problem");
+    }
+}
---
+++ src/test/java/com/cg/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package com.cg;
+
+import org.junit.Test;
+
+import com.cg.MoodAnalyserMain;
+
+import static org.junit.Assert.*;
+
+public class MoodAnalyserTest {
+}
