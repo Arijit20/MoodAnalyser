76981b4077a17be91acf0c38c6a478caebb996c7
Arijit20 <71620955+Arijit20@users.noreply.github.com>
2020-09-30 17:55:43 +0530

    Update MoodAnalyserMain.java

--- src/main/java/com/cg/MoodAnalyserMain.java
+++ src/main/java/com/cg/MoodAnalyserMain.java
@@ -2,13 +2,10 @@ package com.cg;
 
 public class MoodAnalyserMain {
 	public String analyseMood(String message) {
-		try {
 			if (message.contains("sad"))
 				return "SAD";
 			else
 				return "HAPPY";
-		} catch (Exception e) {
-			return "HAPPY";
 		}
 	}
-}
+
