91ae65a8bdbc5ea3c7bf9564989d85225d65bd75
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-30 21:50:45 +0530

    [Arijit]Refactored : Custom Exception Added

--- src/main/java/com/cg/MoodAnalyserMain.java
+++ src/main/java/com/cg/MoodAnalyserMain.java
@@ -10,14 +10,22 @@ public class MoodAnalyserMain {
 		this.message = message;
 	}
 	
-	public String analyseMood() {
+	public String analyseMood(String message) throws MoodAnalyserException{
+		this.message = message;
+		return analyseMood();
+	}
+	
+	public String analyseMood() throws MoodAnalyserException{
 		try {
+		if(message.length() == 0)
+			throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, "Please Enter Some Message");
+		
 			if (message.contains("Happy"))
 				return "SAD";
 			else
 				return "HAPPY";
 		}catch(NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL, "Enter a Proper Message");
 		}
 	}
 }
