296ca44a0b782845d28f278545dab3d48623fa34
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-30 20:03:25 +0530

    [Arijit]Updated : Happy Mood

--- src/test/java/com/cg/MoodAnalyserTest.java
+++ src/test/java/com/cg/MoodAnalyserTest.java
@@ -11,7 +11,7 @@ import org.junit.Assert;
 public class MoodAnalyserTest {
 	@Test
 	public void TestMoodAnalysis() {
-		MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in sad Mood");
+		MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
 		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
