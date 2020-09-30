23b786a25504d1125eeab97cb44a0f19daf0a2b9
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-30 17:29:38 +0530

    [Arijit]Added : Identify Happy Or Sad

--- src/main/java/com/cg/MoodAnalyserMain.java
+++ src/main/java/com/cg/MoodAnalyserMain.java
@@ -1,7 +1,14 @@
 package com.cg;
 
 public class MoodAnalyserMain {
-    public static void main(String[] args) {
-    	System.out.println("Mood Analyser Test Problem");
-    }
+	public String analyseMood(String message) {
+		try {
+			if (message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (Exception e) {
+			return "HAPPY";
+		}
+	}
 }
--- src/test/java/com/cg/MoodAnalyserTest.java
+++ src/test/java/com/cg/MoodAnalyserTest.java
@@ -6,5 +6,13 @@ import com.cg.MoodAnalyserMain;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+
 public class MoodAnalyserTest {
+	@Test
+	public void TestMoodAnalysis() {
+		MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+		String mood = moodAnalyser.analyseMood("This is a happy message");
+		Assert.assertEquals("HAPPY", mood);
+	}
 }
